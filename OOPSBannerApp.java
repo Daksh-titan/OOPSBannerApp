039a4f7e3c69a51d54b19b34ef856b65b6d9e0a6
Daksh-titan <tianisback9@gmail.com>
2026-02-18 05:19:02 +1200

    Update and rename Main.java to OOPSBannerApp.java
    naming tweaks

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,7 +7,7 @@
  * @author Daksh Kumar
  * @version 1.0
  */
-public class Main {
+public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
